326d33a8b46c8c8df02b3dbbfbbd3602f6674076
Adam Luzsi <adamluzsi@gmail.com>
2018-05-27 16:09:26 +0200

    Add presistable interface to the frameless interface collections, this will be used for business entity definitions

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Well in practice, this package makes you be less productive in speed :)
 The goal of the package is the learning and practicing [The Clean Architecture](https://8thlight.com/blog/uncle-bob/2012/08/13/the-clean-architecture.html).
 By doing so you will create web/cli/{{ .ChannelName }} applications that actually external channel and framework independent.
 
-By working disciplined and seperate the scope of your code and enforcing [law of demeter](https://en.wikipedia.org/wiki/Law_of_Demeter) on your architecture,
+By working disciplined and separate the scope of your code and enforcing [law of demeter](https://en.wikipedia.org/wiki/Law_of_Demeter) on your architecture,
 by my you probably end up with something that is boring in term of code, not have fancy.
 
 The results will be something like:
--- frameless.go
+++ frameless.go
@@ -1,3 +1,41 @@
+// Package frameless aims to create convention to build software from ground instead of upside down.
+//
+// But what does this in in practice? This package makes you be less productive in speed for short term! :)
+//
+// The goal of the package is the learning and practicing [The Clean Architecture](https://8thlight.com/blog/uncle-bob/2012/08/13/the-clean-architecture.html).
+// By doing so you will create web/cli/{{ .ChannelName }} applications that actually external channel and framework independent.
+//
+// By working disciplined and separate the scope of your code and enforcing [law of demeter](https://en.wikipedia.org/wiki/Law_of_Demeter) on your architecture,
+// by my you probably end up with something that is boring in term of code, not have fancy.
+//
+// The results will be something like:
+//
+//  * boring code
+//  * separation between
+//    * external interface
+//    * external resource
+//    * template layer
+//    * presentation & serialization
+//    * application control logic
+//      * use cases
+//      * controllers
+//    * Business entities
+//
+// *Yes but how will this help me to achieve this ?*
+//
+// Basically because because the overwhelming possibility to what technology use for a project,
+// sometimes these days reverse the traditional development from ground up to upside down way.
+// So instead of starting to create pure Business rules and business core entities,
+// the developer tricked into start working from End2End through external interface point of view.
+// Such example is when a developer creates an application through testing (sometimes manually) from the webpage point of view,
+// or like "If I click this html button on the side bar, there should be a new record in the database with xy".
+// While it has faster impact in look, usually the business rules rarely created independently from the framework and external resources such as the db.
+//
+// While following the ideologies presented in the project, you will create applications that will be build from ground.
+// You will basically create the pure business entities, than business "use cases"/rules with them,
+// and as a final move, you choose what should be the external interface (cli/mq/http/{{.Channel}}).
+//
+//
 package frameless
 
 import (
@@ -18,6 +56,45 @@ type ControllerFunc func(Presenter, Request) error
 // Serve func implements the Frameless Controller interface
 func (lambda ControllerFunc) Serve(p Presenter, r Request) error {
 	return lambda(p, r)
+}
+
+// Presenter is represent a communication layer presenting layer
+//
+// Scope:
+// 	receive messages, and convert it into a serialized form
+//
+// You should not allow the users of the Presenter object to modify the state of the enwrapped communication channel, such as closing, or direct writing
+type Presenter interface {
+	//
+	// RenderWithTemplate a content on a channel that the Presenter implements
+	//	name helps to determine the what template should be used, but should not include channel specific names
+	//	data is the content that should be used in the template
+	// RenderWithTemplate(name string, data frameless.Content) error
+
+	//
+	// Render renders a simple message back to the enwrapped communication channel
+	//	message is an interface type because the channel communication layer and content and the serialization is up to the Presenter to implement
+	Render(message interface{}) error
+}
+
+// PresenterFunc is a wrapper to convert standalone functions into a presenter
+type PresenterFunc func(interface{}) error
+
+// PresenterBuilder is an example how presenter should be created
+type PresenterBuilder func(io.Writer) Presenter
+
+// Render implements the Presenter Interface
+func (lambda PresenterFunc) Render(message interface{}) error {
+	return lambda(message)
+}
+
+// Request is framework independent way of interacting with a request that has been received on some kind of channel.
+// 	from this, the controller should get all the data and options that should required for the business rule processing
+type Request interface {
+	io.Closer
+	Context() context.Context
+	Options() Getter
+	Data() Iterator
 }
 
 // Fetcher is an object that implements fetching logic for a given Business Entity
@@ -57,41 +134,14 @@ type Iterator interface {
 // IteratorBuilder is a generic example for building iterators how should look
 type IteratorBuilder func(io.ReadCloser) Iterator
 
-// Presenter is represent a communication layer presenting layer
-//
-// Scope:
-// 	receive messages, and convert it into a serialized form
-//
-// You should not allow the users of the Presenter object to modify the state of the enwrapped communication channel, such as closing, or direct writing
-type Presenter interface {
-	//
-	// RenderWithTemplate a content on a channel that the Presenter implements
-	//	name helps to determine the what template should be used, but should not include channel specific names
-	//	data is the content that should be used in the template
-	// RenderWithTemplate(name string, data frameless.Content) error
+// Persistable defines what requirements is expected from a business entity from behavior side if it is marked as a persistable object
+// This interface expected to be used in the Software Application Business Entity definitions.
+type Persistable interface {
+	// Save expects to fulfill the role of "Create" and "Update".
+	// The underling structure that implements the interface is expected to know if it's an already stored object or not.
+	Save() error
 
-	//
-	// Render renders a simple message back to the enwrapped communication channel
-	//	message is an interface type because the channel communication layer and content and the serialization is up to the Presenter to implement
-	Render(message interface{}) error
-}
-
-// PresenterFunc is a wrapper to convert standalone functions into a presenter
-type PresenterFunc func(interface{}) error
-
-// PresenterBuilder is an example how presenter should be created
-type PresenterBuilder func(io.Writer) Presenter
-
-// Render implements the Presenter Interface
-func (lambda PresenterFunc) Render(message interface{}) error {
-	return lambda(message)
-}
-
-// Request is framework independent way of interacting with a request that has been received on some kind of channel.
-// 	from this, the controller should get all the data and options that should required for the business rule processing
-type Request interface {
-	io.Closer
-	Context() context.Context
-	Options() Getter
-	Data() Iterator
+	// Delete is expected to make the object look like deleted from the controller point of view.
+	// The fact that it is deleted actually or just marked as "deleted_at" is up to the implementation.
+	Delete() error
 }
