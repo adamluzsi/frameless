fb1f2903f51ffdb405d49a1f83caa9d618fb8bea
Adam Luzsi <adamluzsi@gmail.com>
2018-06-19 07:17:36 +0200

    simple refactoring

--- reflects/lookupid.go
+++ reflects/lookupid.go
@@ -30,22 +30,3 @@ func lookupByTag(val reflect.Value) (reflect.Value, bool) {
 
 	return val, false
 }
-
-func idReflectValue(val reflect.Value) (reflect.Value, bool) {
-
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
-
-	byName := val.FieldByName("ID")
-	if byName.Kind() != reflect.Invalid {
-		return byName, true
-	}
-
-	byTag, ok := lookupByTag(val)
-	if ok {
-		return byTag, true
-	}
-
-	return reflect.Value{}, false
-}
---
+++ reflects/value.go
@@ -0,0 +1,22 @@
+package reflects
+
+import "reflect"
+
+func idReflectValue(val reflect.Value) (reflect.Value, bool) {
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	byName := val.FieldByName("ID")
+	if byName.Kind() != reflect.Invalid {
+		return byName, true
+	}
+
+	byTag, ok := lookupByTag(val)
+	if ok {
+		return byTag, true
+	}
+
+	return reflect.Value{}, false
+}
