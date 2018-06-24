43604c1900c0a4b1ef81a83675f7e3523d00b344
Adam Luzsi <adamluzsi@gmail.com>
2018-06-24 04:46:59 +0200

    fix

---
+++ spechelper_test.go
@@ -0,0 +1,10 @@
+package frameless_test
+
+import (
+	"testing"
+
+	"github.com/adamluzsi/frameless"
+)
+
+// I had to add this here because the godoc was removing ful example of the query use case
+func (quc InactiveUsers) Test(suite *testing.T, storage frameless.Storage) {}
