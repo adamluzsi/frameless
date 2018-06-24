9dd3d331b5f80b24ad145f9bf468b43fe6e6f49f
Adam Luzsi <adamluzsi@gmail.com>
2018-06-24 22:35:16 +0200

    add request mock for testing

---
+++ requests/mock_test.go
@@ -0,0 +1,26 @@
+package requests_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/adamluzsi/frameless/iterators"
+	"github.com/stretchr/testify/require"
+
+	"github.com/adamluzsi/frameless/requests"
+)
+
+func TestMock(t *testing.T) {
+	t.Parallel()
+
+	m := requests.NewMock(context.Background(), iterators.NewForSingleElement("Hello, World!"))
+	i := m.Data()
+
+	var value string
+	require.Nil(t, iterators.DecodeNext(i, &value))
+	require.Equal(t, "Hello, World!", value)
+
+	require.Nil(t, i.Close())
+	require.Error(t, errors.New("closed"), iterators.DecodeNext(i, &value))
+}
