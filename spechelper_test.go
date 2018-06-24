19ad12b1908ddfec9f8501346e7c9b144c79be25
Adam Luzsi <adamluzsi@gmail.com>
2018-06-24 04:48:20 +0200

    fix

--- spechelper_test.go
+++ spechelper_test.go
@@ -7,4 +7,6 @@ import (
 )
 
 // I had to add this here because the godoc was removing ful example of the query use case
-func (quc InactiveUsers) Test(suite *testing.T, storage frameless.Storage) {}
+func (quc InactiveUsers) Test(suite *testing.T, storage frameless.Storage) {
+	quc.TTest(suite, storage)
+}
