7d7eae1d28e0bc75e49a20fe6031cd38a0a91e28
Adam Luzsi <adamluzsi@gmail.com>
2018-06-30 14:38:59 +0200

    add description to err cases in iterators

--- iterators/decodenext.go
+++ iterators/decodenext.go
@@ -7,7 +7,7 @@ import (
 func DecodeNext(i frameless.Iterator, e frameless.Entity) error {
 
 	if !i.Next() {
-		return ErrNoNextElementFound
+		return ErrNoNextElement
 	}
 
 	return i.Decode(e)
--- iterators/errors.go
+++ iterators/errors.go
@@ -3,6 +3,8 @@ package iterators
 import "errors"
 
 var (
-	ErrClosed             = errors.New("closed")
-	ErrNoNextElementFound = errors.New("no next element found")
+	// ErrClosed is the value that will be returned if a iterator has been closed but next decode is called
+	ErrClosed = errors.New("Closed")
+	// ErrNoNextElement defines that no next element in the iterator, used for iterateover exception cases
+	ErrNoNextElement = errors.New("NoNextElement")
 )
