24265f480455f8407fd0f0e6c604e19a6dc0df78
Adam Luzsi <adamluzsi@gmail.com>
2018-07-04 07:43:52 +0200

    add rules/principles to the repo

--- README.go
+++ README.go
@@ -106,6 +106,46 @@ and you can start create business use cases implementations without any storage.
 
 
 
+Principles that should be followed when you design with this package
+
+Rule 1.
+You can't tell where a program is going to spend its time.
+Bottlenecks occur in surprising places, so don't try to second guess and put in a speed hack until you've proven that's where the bottleneck is.
+
+Rule 2.
+Measure. Don't tune for speed until you've measured, and even then don't unless one part of the code overwhelms the rest.
+
+Rule 3.
+Fancy algorithms are slow when n is small,
+and n is usually small. Fancy algorithms have big constants.
+Until you know that n is frequently going to be big, don't get fancy. (Even if n does get big, use Rule 2 first.)
+
+Rule 4.
+Fancy algorithms are buggier than simple ones, and they're much harder to implement. Use simple algorithms as well as simple data structures.
+
+Rule 5.
+Data dominates. If you've chosen the right data structures and organized things well, the algorithms will almost always be self-evident.
+Data structures, not algorithms, are central to programming.
+
+Rule 6.
+Don't create production code that is not proven to be required.
+Prove it with user story and tests.
+If it is "hard to test", take a break and think over.
+
+Rule 7.
+Use contracts where ever it makes sense concrete type declarations to enforce dependency inversion.
+
+Most of the rules originated from one of my favorite programmer,
+who was the initial reason why I started to read about golang, Rob Pike.
+Pike's rules 1 and 2 restate Tony Hoare's famous maxim "Premature optimization is the root of all evil."
+Ken Thompson rephrased Pike's rules 3 and 4 as "When in doubt, use brute force.".
+Rules 3 and 4 are instances of the design philosophy KISS.
+Rule 5 was previously stated by Fred Brooks in The Mythical Man-Month.
+
+
+
+
+
 Caveat
 
 This research primary targets creating design for business software applications,
