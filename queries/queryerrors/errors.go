427fd375b77914508f522992ab2a5a4478153e45
Adam Luzsi <adamluzsi@gmail.com>
2018-09-29 11:19:36 +0200

    make Exported Error values constant

--- queries/queryerrors/errors.go
+++ queries/queryerrors/errors.go
@@ -1,6 +1,10 @@
-package queries
+package queryerrors
 
-const ErrIDRequired = `
+import "github.com/adamluzsi/frameless/errtype"
+
+const ErrNotFound errtype.Error = "ErrNotFound"
+
+const ErrIDRequired errtype.Error = `
 Can't find the ID in the current structure
 if there is no ID in the subject structure
 custom test needed that explicitly defines how ID is stored and retrived from an entity
