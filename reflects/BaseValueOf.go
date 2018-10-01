9bef5ad4d80bb568c48bab680e7b6cb45d027e46
Adam Luzsi <adamluzsi@gmail.com>
2018-10-01 17:34:36 +0200

    add base value of reflects helper for cases when you have a interface of ptr of struct, and you only need the struct

---
+++ reflects/BaseValueOf.go
@@ -0,0 +1,16 @@
+package reflects
+
+import (
+	"reflect"
+)
+
+
+func BaseValueOf(i interface{}) reflect.Value {
+	v := reflect.ValueOf(i)
+
+	for v.Type().Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	return v
+}
