5fe794314732f587ae2b1f2b5e4a4932d8d2d317
Adam Luzsi <adamluzsi@gmail.com>
2018-10-01 23:57:13 +0200

    enable persist Entity specification

--- queries/persist/Entity.go
+++ queries/persist/Entity.go
@@ -2,6 +2,9 @@ package persist
 
 import (
 	"github.com/adamluzsi/frameless"
+	"github.com/adamluzsi/frameless/queries/fixtures"
+	"github.com/adamluzsi/frameless/storages"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -12,19 +15,19 @@ type Entity struct {
 func (q Entity) Test(t *testing.T, s frameless.Storage, resetDB func()) {
 	t.Run("persist an Entity", func(t *testing.T) {
 
-		//if ID, ok := storages.LookupID(q.Entity); ok && ID != "" {
-		//	t.Fatalf("expected entity shouldn't have any ID yet, but have %s", ID)
-		//}
-		//
-		//e := fixtures.New(q.Entity)
-		//i := s.Exec(Entity{Entity: e})
-		//
-		//require.NotNil(t, i)
-		//require.Nil(t, i.Err())
-		//
-		//ID, ok := storages.LookupID(q.Entity)
-		//require.True(t, ok, "ID is not defined in the entity struct src definition")
-		//require.True(t, len(ID) > 0, "it's expected that storage set the storage ID in the entity")
+		if ID, _ := storages.LookupID(q.Entity); ID != "" {
+			t.Fatalf("expected entity shouldn't have any ID yet, but have %s", ID)
+		}
+
+		e := fixtures.New(q.Entity)
+		i := s.Exec(Entity{Entity: e})
+
+		require.NotNil(t, i)
+		require.Nil(t, i.Err())
+
+		ID, ok := storages.LookupID(e)
+		require.True(t, ok, "ID is not defined in the entity struct src definition")
+		require.True(t, len(ID) > 0, "it's expected that storage set the storage ID in the entity")
 
 	})
 }
