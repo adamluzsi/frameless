981c0065dad350583864bb3ca58d8ca7af9c004b
Adam Luzsi <adamluzsi@gmail.com>
2018-10-01 23:56:42 +0200

    make LookupID of storage works with recursively embedded pointers as well (for query testing scenarios)

--- storages/LookupID.go
+++ storages/LookupID.go
@@ -1,12 +1,10 @@
 package storages
 
-import (
-	"reflect"
-)
+import "github.com/adamluzsi/frameless/reflects"
 
 func LookupID(i interface{}) (string, bool) {
 
-	val, ok := idReflectValue(reflect.ValueOf(i))
+	val, ok := idReflectValue(reflects.BaseValueOf(i))
 
 	if ok {
 		return val.String(), true
--- storages/LookupID_test.go
+++ storages/LookupID_test.go
@@ -25,6 +25,21 @@ func TestLookupID_PointerIDGivenByFieldName_IDReturned(t *testing.T) {
 	require.Equal(t, "ok", id)
 }
 
+func TestLookupID_PointerOfPointerIDGivenByFieldName_IDReturned(t *testing.T) {
+	t.Parallel()
+
+	var ptr1 *IDInFieldName
+	var ptr2 **IDInFieldName
+
+	ptr1 = &IDInFieldName{"ok"}
+	ptr2 = &ptr1
+
+	id, ok := storages.LookupID(ptr2)
+
+	require.True(t, ok)
+	require.Equal(t, "ok", id)
+}
+
 func TestLookupID_IDGivenByTag_IDReturned(t *testing.T) {
 	t.Parallel()
 
