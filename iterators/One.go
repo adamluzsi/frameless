0ad47d0c0a5695cbdbe2f160e66da3fecfc7021a
Adam Luzsi <adamluzsi@gmail.com>
2018-10-06 04:23:33 +0200

    create One helper method in terators for cases where you only expect one value from an iterator

---
+++ iterators/One.go
@@ -0,0 +1,25 @@
+package iterators
+
+import (
+	"github.com/adamluzsi/frameless"
+)
+
+func One(i frameless.Iterator, e frameless.Entity) error  {
+	if err := i.Err(); err != nil {
+		return err
+	}
+
+	if !i.Next() {
+		return ErrNoNextElement
+	}
+
+	if err := i.Decode(e); err != nil {
+		return err
+	}
+
+	if i.Next() {
+		return ErrUnexpectedNextElement
+	}
+
+	return i.Err()
+}
--- iterators/errors.go
+++ iterators/errors.go
@@ -9,4 +9,6 @@ var (
 	ErrClosed frameless.Error = "Closed"
 	// ErrNoNextElement defines that no next element in the iterator, used for iterateover exception cases
 	ErrNoNextElement frameless.Error = "NoNextElement"
+	// ErrUnexpectedNextElement is an error for the cases when it is an explicit requirement that only one element should be returned
+	ErrUnexpectedNextElement frameless.Error = "ErrUnexpectedNextElement"
 )
