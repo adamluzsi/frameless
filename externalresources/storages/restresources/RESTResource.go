0ee740c3d87de3de8f1a6aa92a93213cda9657bb
Adam Luzsi <adamluzsi@gmail.com>
2018-10-13 21:32:31 +0200

    sketch for restful external resource

--- externalresources/storages/restresources/RESTResource.go
+++ externalresources/storages/restresources/RESTResource.go
@@ -1,6 +1,36 @@
-package httpresource
+package restresources
 
-type HTTPResource struct {
+import (
+	"fmt"
+	"github.com/adamluzsi/frameless"
+	"github.com/adamluzsi/frameless/iterators"
+	"github.com/adamluzsi/frameless/queries/find"
+	"github.com/adamluzsi/frameless/queries/queryerrors"
+)
 
+func NewRESTResource(url string) *RESTResource {
+	return &RESTResource{url: url}
 }
+
+type RESTResource struct{
+	url string
+}
+
+func (*RESTResource) Exec(query frameless.Query) frameless.Iterator {
+	switch data := query.(type) {
+
+	case find.ByID:
+		fmt.Println(data)
+		return nil
+
+	default:
+		return iterators.NewError(queryerrors.ErrNotImplemented)
+	}
+}
+
+func (*RESTResource) Close() error {
+	return nil
+}
+
+
 
