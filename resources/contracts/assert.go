a228ae1dc857770d18c712a0cdb339e7ac7b6528
Adam Luzsi <adamluzsi@gmail.com>
2021-04-14 00:32:24 +0200

    refact contract assertions

--- resources/contracts/assert.go
+++ resources/contracts/assert.go
@@ -20,6 +20,7 @@ var Waiter = testcase.Waiter{
 var AsyncTester = testcase.Retry{Strategy: Waiter}
 
 func HasID(tb testing.TB, ent interface{}) (id interface{}) {
+	tb.Helper()
 	AsyncTester.Assert(tb, func(tb testing.TB) {
 		var ok bool
 		id, ok = resources.LookupID(ent)
@@ -30,6 +31,7 @@ func HasID(tb testing.TB, ent interface{}) (id interface{}) {
 }
 
 func IsFindable(tb testing.TB, T T, subject resources.Finder, ctx context.Context, id interface{}) interface{} {
+	tb.Helper()
 	var ptr interface{}
 	newFn := newEntityFunc(T)
 	AsyncTester.Assert(tb, func(tb testing.TB) {
@@ -42,6 +44,7 @@ func IsFindable(tb testing.TB, T T, subject resources.Finder, ctx context.Contex
 }
 
 func IsAbsent(tb testing.TB, T T, subject resources.Finder, ctx context.Context, id interface{}) {
+	tb.Helper()
 	n := newEntityFunc(T)
 	AsyncTester.Assert(tb, func(tb testing.TB) {
 		found, err := subject.FindByID(ctx, n(), id)
@@ -51,14 +54,19 @@ func IsAbsent(tb testing.TB, T T, subject resources.Finder, ctx context.Context,
 }
 
 func HasEntity(tb testing.TB, subject resources.Finder, ctx context.Context, ent interface{}) {
+	tb.Helper()
 	T := toT(ent)
 	id := HasID(tb, ent)
 	AsyncTester.Assert(tb, func(tb testing.TB) {
+		// IsFindable yields the currently found value
+		// that might be not yet the value we expect to see
+		// so the .Assert block ensure multiple tries
 		require.Equal(tb, ent, IsFindable(tb, T, subject, ctx, id))
 	})
 }
 
 func CreateEntity(tb testing.TB, subject CRD, ctx context.Context, ptr interface{}) {
+	tb.Helper()
 	T := toT(ptr)
 	require.Nil(tb, subject.Create(ctx, ptr))
 	id := HasID(tb, ptr)
@@ -71,8 +79,12 @@ func UpdateEntity(tb testing.TB, subject interface {
 	resources.Updater
 	resources.Deleter
 }, ctx context.Context, ptr interface{}) {
+	tb.Helper()
 	T := toT(ptr)
 	id, _ := resources.LookupID(ptr)
+	// IsFindable ensures that by the time Update is executed,
+	// the entity is present in the resource.
+	IsFindable(tb, T, subject, ctx, id)
 	require.Nil(tb, subject.Update(ctx, ptr))
 	AsyncTester.Assert(tb, func(tb testing.TB) {
 		entity := IsFindable(tb, T, subject, ctx, id)
@@ -81,6 +93,7 @@ func UpdateEntity(tb testing.TB, subject interface {
 }
 
 func DeleteEntity(tb testing.TB, subject CRD, ctx context.Context, ent interface{}) {
+	tb.Helper()
 	T := toT(ent)
 	id := HasID(tb, ent)
 	IsFindable(tb, T, subject, ctx, id)
@@ -89,6 +102,7 @@ func DeleteEntity(tb testing.TB, subject CRD, ctx context.Context, ent interface
 }
 
 func DeleteAllEntity(tb testing.TB, subject CRD, ctx context.Context, T resources.T) {
+	tb.Helper()
 	require.Nil(tb, subject.DeleteAll(ctx, T))
 	Waiter.Wait() // TODO: FIXME: race condition between tests might depend on this
 	AsyncTester.Assert(tb, func(tb testing.TB) {
