f6d8b43d6632cb497ec4ec5b4f728ebebad33a64
Adam Luzsi <adamluzsi@gmail.com>
2022-08-21 22:13:26 +0200

    refact pipe iterator

--- iterators/Pipe.go
+++ iterators/Pipe.go
@@ -3,19 +3,28 @@ package iterators
 // Pipe return a receiver and a sender.
 // This can be used with resources that
 func Pipe[T any]() (*PipeIn[T], *PipeOut[T]) {
-	valueChan := make(chan T)
-	doneChan := make(chan struct{}, 1)
-	errChan := make(chan error, 1)
-	return &PipeIn[T]{ValueChan: valueChan, DoneChan: doneChan, ErrChan: errChan},
-		&PipeOut[T]{ValueChan: valueChan, DoneChan: doneChan, ErrChan: errChan}
+	pipeChan := makePipeChan[T]()
+	return &PipeIn[T]{pipeChan: pipeChan},
+		&PipeOut[T]{pipeChan: pipeChan}
+}
+
+func makePipeChan[T any]() pipeChan[T] {
+	return pipeChan[T]{
+		values: make(chan T),
+		done:   make(chan struct{}, 1),
+		err:    make(chan error, 1),
+	}
+}
+
+type pipeChan[T any] struct {
+	values chan T
+	done   chan struct{}
+	err    chan error
 }
 
 // PipeOut implements iterator interface while it's still being able to receive values, used for streaming
 type PipeOut[T any] struct {
-	ValueChan <-chan T
-	DoneChan  chan<- struct{}
-	ErrChan   <-chan error
-
+	pipeChan[T]
 	value   T
 	lastErr error
 }
@@ -23,15 +32,15 @@ type PipeOut[T any] struct {
 // Close sends a signal back that no more value should be sent because receiver stop listening
 func (i *PipeOut[T]) Close() error {
 	defer func() { recover() }()
-	i.DoneChan <- struct{}{}
-	close(i.DoneChan)
+	i.done <- struct{}{}
+	close(i.done)
 	return nil
 }
 
 // Next set the current entity for the next value
 // returns false if no next value
 func (i *PipeOut[T]) Next() bool {
-	v, ok := <-i.ValueChan
+	v, ok := <-i.pipeChan.values
 	if !ok {
 		return false
 	}
@@ -42,7 +51,7 @@ func (i *PipeOut[T]) Next() bool {
 
 // Err returns an error object that the pipe sender want to present for the pipe receiver
 func (i *PipeOut[T]) Err() error {
-	err, ok := <-i.ErrChan
+	err, ok := <-i.err
 	if ok {
 		i.lastErr = err
 	}
@@ -50,43 +59,41 @@ func (i *PipeOut[T]) Err() error {
 	return i.lastErr
 }
 
-// Decode will link the current buffered value to the pointer value that is given as "e"
+// Value will link the current buffered value to the pointer value that is given as "e"
 func (i *PipeOut[T]) Value() T {
 	return i.value
 }
 
 // PipeIn provides access to feed a pipe receiver with entities
 type PipeIn[T any] struct {
-	ValueChan chan<- T
-	DoneChan  <-chan struct{}
-	ErrChan   chan<- error
+	pipeChan[T]
 }
 
-// Encode send value to the PipeOut
-// and returns ErrClosed error if no more value expected on the receiver side
+// Value send value to the PipeOut.Value.
+// It returns if sending was possible.
 func (f *PipeIn[T]) Value(v T) (ok bool) {
 	select {
-	case f.ValueChan <- v:
+	case f.pipeChan.values <- v:
 		return true
-	case <-f.DoneChan:
+	case <-f.pipeChan.done:
 		return false
 	}
 }
 
-// ErrorIter send an error object to the PipeOut side, so it will be accessible with iterator.Err()
+// Error send an error object to the PipeOut side, so it will be accessible with iterator.Err()
 func (f *PipeIn[T]) Error(err error) {
 	if err == nil {
 		return
 	}
 
 	defer func() { recover() }()
-	f.ErrChan <- err
+	f.pipeChan.err <- err
 }
 
-// Close close the feed and err channel, which eventually notify the receiver that no more value expected
+// Close will close the feed and err channels, which eventually notify the receiver that no more value expected
 func (f *PipeIn[T]) Close() error {
 	defer func() { recover() }()
-	close(f.ValueChan)
-	close(f.ErrChan)
+	close(f.pipeChan.values)
+	close(f.pipeChan.err)
 	return nil
 }
