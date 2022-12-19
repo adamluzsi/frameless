6c44375890a87a0c535cf933c93665c1109dabfc
Adam Luzsi <adamluzsi@gmail.com>
2022-12-19 22:27:13 +0100

    add ranges package to make the creation of iterable ranges easy

---
+++ ports/iterators/ranges/Int_test.go
@@ -0,0 +1,82 @@
+package ranges_test
+
+import (
+	"fmt"
+	"github.com/adamluzsi/frameless/ports/iterators"
+	iteratorcontracts "github.com/adamluzsi/frameless/ports/iterators/contracts"
+	"github.com/adamluzsi/frameless/ports/iterators/ranges"
+	"github.com/adamluzsi/testcase"
+	"github.com/adamluzsi/testcase/assert"
+	"testing"
+)
+
+func ExampleInt() {
+	iter := ranges.Int(1, 9)
+	defer iter.Close()
+
+	for iter.Next() {
+		// prints characters between 1 and 9
+		// 1, 2, 3, 4, 5, 6, 7, 8, 9
+		fmt.Println(iter.Value())
+	}
+
+	if err := iter.Err(); err != nil {
+		panic(err.Error())
+	}
+}
+
+func TestInt_smoke(t *testing.T) {
+	it := assert.MakeIt(t)
+
+	vs, err := iterators.Collect(ranges.Int(1, 9))
+	it.Must.NoError(err)
+	it.Must.Equal([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, vs)
+
+	vs, err = iterators.Collect(ranges.Int(4, 7))
+	it.Must.NoError(err)
+	it.Must.Equal([]int{4, 5, 6, 7}, vs)
+
+	vs, err = iterators.Collect(ranges.Int(5, 1))
+	it.Must.NoError(err)
+	it.Must.Equal([]int{}, vs)
+}
+
+func TestInt(t *testing.T) {
+	s := testcase.NewSpec(t)
+
+	var (
+		begin = testcase.Let(s, func(t *testcase.T) int {
+			return t.Random.IntB(3, 7)
+		})
+		end = testcase.Let(s, func(t *testcase.T) int {
+			return t.Random.IntB(8, 13)
+		})
+	)
+	subject := testcase.Let(s, func(t *testcase.T) iterators.Iterator[int] {
+		return ranges.Int(begin.Get(t), end.Get(t))
+	})
+
+	s.Then("it returns an iterator that contains the defined numeric range from min to max", func(t *testcase.T) {
+		actual, err := iterators.Collect(subject.Get(t))
+		t.Must.NoError(err)
+
+		var expected []int
+		for i := begin.Get(t); i <= end.Get(t); i++ {
+			expected = append(expected, i)
+		}
+
+		t.Must.NotEmpty(expected)
+		t.Must.Equal(expected, actual)
+	})
+}
+
+func TestInt_implementsIterator(t *testing.T) {
+	iteratorcontracts.Iterator[int]{
+		MakeSubject: func(tb testing.TB) iterators.Iterator[int] {
+			t := testcase.ToT(&tb)
+			min := t.Random.IntB(3, 7)
+			max := t.Random.IntB(8, 13)
+			return ranges.Int(min, max)
+		},
+	}.Test(t)
+}
