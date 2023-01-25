89c46847cb3502165bb2612fd463bc4f7e9f80cc
Adam Luzsi <adamluzsi@gmail.com>
2023-01-25 20:30:08 +0100

    enum pkg

---
+++ pkg/enum/enum.go
@@ -0,0 +1,165 @@
+package enum
+
+import (
+	"fmt"
+	"github.com/adamluzsi/frameless/pkg/errorutil"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+var ErrInvalid = errorutil.UserError{
+	ID:      "enum-invalid-value",
+	Message: "The value does not match the enumerator specification",
+}
+
+func ValidateStruct(v any) error {
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Struct {
+		return fmt.Errorf("only struct types are supported. (%T)", v)
+	}
+
+	rt := rv.Type()
+	for i, fnum := 0, rt.NumField(); i < fnum; i++ {
+		field := rt.Field(i)
+		value := rv.Field(i)
+
+		if tag, ok := field.Tag.Lookup(structTagName); ok {
+			enumerators, err := parseTag(field.Type, tag)
+			if err != nil {
+				return err
+			}
+
+			if !match(value, enumerators) {
+				return ErrInvalid.With().Detailf("%v does not match enumerator specification: %s", v, tag)
+			}
+		}
+	}
+	return nil
+}
+
+func match(rv reflect.Value, enumerators []reflect.Value) bool {
+	if len(enumerators) == 0 {
+		return true
+	}
+	switch rv.Type().Kind() {
+	case reflect.Slice:
+		for i, length := 0, rv.Len(); i < length; i++ {
+			if !match(rv.Index(i), enumerators) {
+				return false
+			}
+		}
+		return true
+	default:
+		v := rv.Interface()
+		for _, enumerator := range enumerators {
+			if enumerator.Interface() == v {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+const structTagName = "enum"
+
+func parseTag(rt reflect.Type, raw string) ([]reflect.Value, error) {
+	const osMaxBitSupport = 64
+
+	if len(raw) == 0 {
+		return nil, nil
+	}
+
+	switch rt.Kind() {
+	case reflect.Slice:
+		return parseTag(rt.Elem(), raw)
+	}
+
+	chars := []rune(raw)
+	sepCharPos := len(chars) - 1
+	separator := string(chars[sepCharPos:])
+	elements := strings.Split(string(chars[:sepCharPos]), separator)
+
+	switch rt.Kind() {
+	case reflect.String:
+		return mapVS(elements, rt, func(s string) (reflect.Value, error) {
+			return reflect.ValueOf(s), nil
+		})
+
+	case reflect.Bool:
+		return mapVS(elements, rt, func(s string) (reflect.Value, error) {
+			b, err := strconv.ParseBool(s)
+			return reflect.ValueOf(b), err
+		})
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return mapVS(elements, rt, func(s string) (reflect.Value, error) {
+			var bitSize = osMaxBitSupport
+			switch rt.Kind() {
+			case reflect.Int:
+				bitSize = osMaxBitSupport
+			case reflect.Int8:
+				bitSize = 8
+			case reflect.Int16:
+				bitSize = 16
+			case reflect.Int32:
+				bitSize = 32
+			case reflect.Int64:
+				bitSize = 64
+			}
+			n, err := strconv.ParseInt(s, 10, bitSize)
+			return reflect.ValueOf(n), err
+		})
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return mapVS(elements, rt, func(s string) (reflect.Value, error) {
+			var bitSize = osMaxBitSupport
+			switch rt.Kind() {
+			case reflect.Uint:
+				bitSize = osMaxBitSupport
+			case reflect.Uint8:
+				bitSize = 8
+			case reflect.Uint16:
+				bitSize = 16
+			case reflect.Uint32:
+				bitSize = 32
+			case reflect.Uint64:
+				bitSize = 64
+			}
+			b, err := strconv.ParseUint(s, 10, bitSize)
+			return reflect.ValueOf(b), err
+		})
+
+	case reflect.Float32, reflect.Float64:
+		return mapVS(elements, rt, func(s string) (reflect.Value, error) {
+			var bitSize = osMaxBitSupport
+			switch rt.Kind() {
+			case reflect.Float32:
+				bitSize = 32
+			case reflect.Float64:
+				bitSize = 64
+			}
+			float, err := strconv.ParseFloat(s, bitSize)
+			return reflect.ValueOf(float), err
+		})
+
+	default:
+		return nil, fmt.Errorf("enum is not supported for %T", rt.String())
+	}
+}
+
+func mapVS(vs []string, rt reflect.Type, transform func(string) (reflect.Value, error)) ([]reflect.Value, error) {
+	var out []reflect.Value
+	for _, v := range vs {
+		value, err := transform(v)
+		if err != nil {
+			return nil, err
+		}
+		if !value.CanConvert(rt) {
+			return nil, fmt.Errorf("%T is not converatble to %T", value.Type().String(), rt.String())
+		}
+		value = value.Convert(rt)
+		out = append(out, value)
+	}
+	return out, nil
+}
---
+++ pkg/enum/enum_test.go
@@ -0,0 +1,267 @@
+package enum_test
+
+import (
+	"github.com/adamluzsi/frameless/pkg/enum"
+	"github.com/adamluzsi/testcase"
+	"github.com/adamluzsi/testcase/assert"
+	"testing"
+)
+
+func TestValidateStruct_smoke(t *testing.T) {
+	type (
+		EmptyEnumExample struct {
+			V string `enum:""`
+		}
+		UnsupportedEnumExample struct {
+			V func() `enum:";"`
+		}
+		InvalidExample struct {
+			V int `enum:"hello;world;"`
+		}
+		StringExample struct {
+			V string `enum:"A|B|C|"`
+		}
+		IntExample struct {
+			V int `enum:"42|24|"`
+		}
+		Int8Example struct {
+			V int8 `enum:"42,24,"`
+		}
+		Int16Example struct {
+			V int16 `enum:"42;24;"`
+		}
+		Int32Example struct {
+			V int32 `enum:"42/24/"`
+		}
+		Int64Example struct {
+			V int64 `enum:"42/24/"`
+		}
+		UIntExample struct {
+			V uint `enum:"42|24|"`
+		}
+		UInt8Example struct {
+			V uint8 `enum:"42,24,"`
+		}
+		UInt16Example struct {
+			V uint16 `enum:"42;24;"`
+		}
+		UInt32Example struct {
+			V uint32 `enum:"42/24/"`
+		}
+		UInt64Example struct {
+			V uint64 `enum:"42/24/"`
+		}
+		Float32Example struct {
+			V float32 `enum:"42.24 24.42 "`
+		}
+		Float64Example struct {
+			V float64 `enum:"42.24;24.42;"`
+		}
+		BoolExample struct {
+			V bool `enum:"true;"`
+		}
+		SubStringType        string
+		StringSubTypeExample struct {
+			V SubStringType `enum:"A;B;C;"`
+		}
+		SliceExample struct {
+			V []string `enum:"A;B;C;"`
+		}
+	)
+
+	type Case struct {
+		V     any
+		IsErr bool
+	}
+	cases := map[string]Case{
+		"on non struct value type, validation fails": {
+			V:     "Hello, world!",
+			IsErr: true,
+		},
+
+		"on empty enum list, everything is accepted": {
+			V:     EmptyEnumExample{V: "foo/bar/baz"},
+			IsErr: false,
+		},
+
+		"on unsupported enum list, error returned": {
+			V:     UnsupportedEnumExample{V: func() {}},
+			IsErr: true,
+		},
+
+		"on invalid enumerator type, error returned": {
+			V:     InvalidExample{V: 42},
+			IsErr: true,
+		},
+
+		"on sub type, when value match the enum list": {
+			V:     StringSubTypeExample{V: "A"},
+			IsErr: false,
+		},
+		"on sub type, when value doesn't match the enum list": {
+			V:     StringSubTypeExample{V: "foo"},
+			IsErr: true,
+		},
+
+		"bool - match enum list": {
+			V:     BoolExample{V: true},
+			IsErr: false,
+		},
+		"bool - doesn't match enum list": {
+			V:     BoolExample{V: false},
+			IsErr: true,
+		},
+
+		"string - match enum list - enum pos 1": {
+			V:     StringExample{V: "A"},
+			IsErr: false,
+		},
+		"string - match enum list - enum pos 2": {
+			V:     StringExample{V: "B"},
+			IsErr: false,
+		},
+		"string - doesn't match enum list - invalid value": {
+			V:     StringExample{V: "128"},
+			IsErr: true,
+		},
+		"string - doesn't match enum list - zero value, when zero is not registered as valid value": {
+			V:     StringExample{},
+			IsErr: true,
+		},
+
+		"int - match enum list": {
+			V:     IntExample{V: 42},
+			IsErr: false,
+		},
+		"int - doesn't match enum list": {
+			V:     IntExample{V: 128},
+			IsErr: true,
+		},
+
+		"int8 - match enum list": {
+			V:     Int8Example{V: 42},
+			IsErr: false,
+		},
+		"int8 - doesn't match enum list": {
+			V:     Int8Example{V: 16},
+			IsErr: true,
+		},
+
+		"int16 - match enum list": {
+			V:     Int16Example{V: 42},
+			IsErr: false,
+		},
+		"int16 - doesn't match enum list": {
+			V:     Int16Example{V: 128},
+			IsErr: true,
+		},
+
+		"int32 - match enum list": {
+			V:     Int32Example{V: 42},
+			IsErr: false,
+		},
+		"int32 - doesn't match enum list": {
+			V:     Int32Example{V: 128},
+			IsErr: true,
+		},
+
+		"int64 - match enum list": {
+			V:     Int64Example{V: 42},
+			IsErr: false,
+		},
+		"int64 - doesn't match enum list": {
+			V:     Int64Example{V: 128},
+			IsErr: true,
+		},
+
+		"uint - match enum list": {
+			V:     UIntExample{V: 42},
+			IsErr: false,
+		},
+		"uint - doesn't match enum list": {
+			V:     UIntExample{V: 128},
+			IsErr: true,
+		},
+
+		"uint8 - match enum list": {
+			V:     UInt8Example{V: 42},
+			IsErr: false,
+		},
+		"uint8 - doesn't match enum list": {
+			V:     UInt8Example{V: 16},
+			IsErr: true,
+		},
+
+		"uint16 - match enum list": {
+			V:     UInt16Example{V: 42},
+			IsErr: false,
+		},
+		"uint16 - doesn't match enum list": {
+			V:     UInt16Example{V: 128},
+			IsErr: true,
+		},
+
+		"uint32 - match enum list": {
+			V:     UInt32Example{V: 42},
+			IsErr: false,
+		},
+		"uint32 - doesn't match enum list": {
+			V:     UInt32Example{V: 128},
+			IsErr: true,
+		},
+
+		"uint64 - match enum list": {
+			V:     UInt64Example{V: 42},
+			IsErr: false,
+		},
+		"uint64 - doesn't match enum list": {
+			V:     UInt64Example{V: 128},
+			IsErr: true,
+		},
+
+		"float32 - match enum list": {
+			V:     Float32Example{V: 42.24},
+			IsErr: false,
+		},
+		"float32 - doesn't match enum list": {
+			V:     Float32Example{V: 42.42},
+			IsErr: true,
+		},
+
+		"float64 - match enum list": {
+			V:     Float64Example{V: 42.24},
+			IsErr: false,
+		},
+		"float64 - doesn't match enum list": {
+			V:     Float64Example{V: 42.42},
+			IsErr: true,
+		},
+
+		"slice - values match the enum list": {
+			V:     SliceExample{V: []string{"A", "C"}},
+			IsErr: false,
+		},
+		"slice - values doesn't match the enum list": {
+			V:     SliceExample{V: []string{"A", "foo"}},
+			IsErr: true,
+		},
+	}
+
+	testcase.TableTest(t, cases, func(t *testcase.T, c Case) {
+		gotErr := enum.ValidateStruct(c.V)
+
+		if c.IsErr {
+			t.Must.Error(gotErr)
+		} else {
+			t.Must.NoError(gotErr)
+		}
+	})
+
+	t.Run("test position", func(t *testing.T) {
+		assert.Error(t, enum.ValidateStruct(StringExample{}))
+		assert.Error(t, enum.ValidateStruct(StringExample{V: "42"}))
+		assert.NoError(t, enum.ValidateStruct(StringExample{V: "A"}))
+		assert.NoError(t, enum.ValidateStruct(StringExample{V: "B"}))
+		assert.NoError(t, enum.ValidateStruct(StringExample{V: "C"}))
+	})
+}
