6b8e973d54d48d21a8162a5ca29bd0586e3aca7a
Adam Luzsi <adamluzsi@gmail.com>
2023-04-11 23:31:33 +0200

    add support for easy initialisation with pointer types

--- pkg/pointer/pointer.go
+++ pkg/pointer/pointer.go
@@ -1,10 +1,78 @@
 package pointer
 
+import (
+	"fmt"
+	"sync"
+	"unsafe"
+)
+
+// Of takes the pointer of a value.
 func Of[T any](v T) *T { return &v }
 
+// Deref will return the referenced value,
+// or if the pointer has no value,
+// then it returns with the zero value.
 func Deref[T any](v *T) T {
 	if v == nil {
 		return *new(T)
 	}
 	return *v
+}
+
+// Init will initialise a variable **T by first checking its value,
+// and if it's not set, it assigns a default value to it.
+// Init is safe to use concurrently, it has no race condition.
+func Init[T any, DefaultValue func() T | *T](v **T, init DefaultValue) T {
+	if v == nil {
+		panic(fmt.Sprintf("nil pointer exception with pointers.Init for %T", *new(T)))
+	}
+	if val, ok := initFastPath(v); ok {
+		return *val
+	}
+	defer initsync(v)()
+	if *v != nil {
+		return **v
+	}
+	switch dv := any(init).(type) {
+	case func() T:
+		*v = Of(dv())
+	case *T:
+		*v = Of(*dv)
+	}
+	return **v
+}
+
+func initFastPath[T any](v **T) (*T, bool) {
+	initlcks.Mutex.RLock()
+	defer initlcks.Mutex.RUnlock()
+	if *v != nil {
+		return *v, true
+	}
+	return nil, false
+}
+
+var initlcks = struct {
+	Mutex sync.RWMutex
+	Map   map[uintptr]*sync.Mutex
+}{Map: map[uintptr]*sync.Mutex{}}
+
+func initsync[T any](v **T) func() {
+	key := uintptr(unsafe.Pointer(v))
+	initlcks.Mutex.Lock()
+	m, ok := initlcks.Map[key]
+	if !ok {
+		m = &sync.Mutex{}
+		initlcks.Map[key] = m
+	}
+	initlcks.Mutex.Unlock()
+	m.Lock()
+	return func() {
+		m.Unlock()
+		if ok {
+			return
+		}
+		initlcks.Mutex.Lock()
+		defer initlcks.Mutex.Unlock()
+		delete(initlcks.Map, key)
+	}
 }
--- pkg/pointer/pointer_test.go
+++ pkg/pointer/pointer_test.go
@@ -9,11 +9,29 @@ import (
 	"github.com/adamluzsi/testcase/random"
 )
 
+type ExampleStruct struct {
+	StrPtrField *string
+	IntPtrField *int
+}
+
+func ExampleOf() {
+	_ = ExampleStruct{
+		StrPtrField: pointer.Of("42"),
+		IntPtrField: pointer.Of(42),
+	}
+}
+
 func TestOf(tt *testing.T) {
 	t := testcase.ToT(tt)
 	var value = t.Random.String()
 	vptr := pointer.Of(value)
 	t.Must.Equal(&value, vptr)
+}
+
+func ExampleDeref() {
+	var es ExampleStruct
+	_ = pointer.Deref(es.StrPtrField)
+	_ = pointer.Deref(es.IntPtrField)
 }
 
 func TestDeref(t *testing.T) {
@@ -27,5 +45,95 @@ func TestDeref(t *testing.T) {
 		expected := rnd.String()
 		got := pointer.Deref(&expected)
 		assert.Equal[string](t, expected, got)
+	})
+}
+
+func ExampleInit() {
+	type MyType struct {
+		V *string
+	}
+	var mt MyType
+
+	_ = pointer.Init(&mt.V, func() string {
+		return "default value from a lambda"
+	})
+
+	_ = pointer.Init(&mt.V, pointer.Of("default value from a pointer"))
+}
+
+func TestInit(t *testing.T) {
+	rnd := random.New(random.CryptoSeed{})
+	t.Run("on nil value, value is constructed from a func", func(t *testing.T) {
+		var str *string
+		exp := rnd.String()
+		got := pointer.Init(&str, func() string { return exp })
+		assert.Equal[string](t, exp, got)
+		assert.Equal[string](t, exp, *str)
+	})
+	t.Run("on nil value, fallback default value is set as the value", func(t *testing.T) {
+		var str *string
+		exp := rnd.String()
+		got := pointer.Init(&str, &exp)
+		assert.Equal[string](t, exp, got)
+		assert.NotNil(t, str)
+		assert.Equal[string](t, exp, *str)
+
+		t.Run("the used default value can't be mutated with the initialised pointer value", func(t *testing.T) {
+			uid := rnd.UUID()
+			*str = uid
+			assert.NotEqual(t, uid, exp)
+		})
+	})
+	t.Run("on non nil value, actual value returned", func(t *testing.T) {
+		expected := rnd.String()
+		var str *string
+		str = &expected
+		got := pointer.Init(&str, func() string { return "42" })
+		assert.Equal[string](t, expected, got)
+		assert.NotNil(t, str)
+		assert.Equal[string](t, expected, *str)
+	})
+	t.Run("supports embedded initialisation", func(t *testing.T) {
+		expected := rnd.String()
+		var str1, str2 *string
+		got := pointer.Init(&str1, func() string {
+			return pointer.Init(&str2, func() string {
+				return expected
+			})
+		})
+		assert.Equal[string](t, expected, got)
+		assert.NotNil(t, str1)
+		assert.Equal[string](t, expected, *str1)
+		assert.NotNil(t, str2)
+		assert.Equal[string](t, expected, *str2)
+	})
+}
+
+func TestInit_race(t *testing.T) {
+	rnd := random.New(random.CryptoSeed{})
+	var str *string
+
+	blk := func() {
+		_ = pointer.Init(&str, func() string { return "42" })
+	}
+	more := random.Slice[func()](rnd.IntB(128, 1024), func() func() { return blk })
+	testcase.Race(blk, blk, more...)
+}
+
+func BenchmarkInit(b *testing.B) {
+	initFunc := func() string { return "42" }
+	b.Run("when init required", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			var str *string
+			_ = pointer.Init(&str, initFunc)
+		}
+	})
+	b.Run("when init is not required", func(b *testing.B) {
+		var str *string
+		pointer.Init(&str, initFunc)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			_ = pointer.Init(&str, initFunc)
+		}
 	})
 }
