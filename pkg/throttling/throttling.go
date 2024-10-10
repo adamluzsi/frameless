d634c893c3ac7ac42e086ae16d6ca4b4cbebcddb
Adam Luzsi <adamluzsi@gmail.com>
2024-10-10 15:33:57 +0200

    wip

--- pkg/throttling/throttling.go
+++ pkg/throttling/throttling.go
@@ -2,7 +2,12 @@ package throttling
 
 import (
 	"context"
+	"fmt"
+	"sync"
 	"time"
+
+	"go.llib.dev/frameless/pkg/slicekit"
+	"go.llib.dev/testcase/clock"
 )
 
 type ThrottlingStrategy interface {
@@ -16,9 +21,36 @@ type ThrottlingStrategy interface {
 // Once the limit is reached, further requests in that window are throttled until the next window begins.
 type FixedWindow struct {
 	Rate Rate
+
+	events events
 }
 
-func (ts *FixedWindow) Throttle(context.Context) error {
+func (fw *FixedWindow) Throttle(ctx context.Context) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
+	if fw.Rate.IsZero() {
+		return nil
+	}
+
+	stats := fw.events.Tick(fw.Rate)
+	isRateLimited := fw.Rate.N < stats.N
+
+	// If the stats.N is greater than or equal to fw.Rate.N, it means the rate limit has been reached.
+	if isRateLimited {
+		// Calculate the time until the next window starts.
+		nextWindowEnd := stats.FirstAt.Add(fw.Rate.Per)
+		remainingTime := nextWindowEnd.Sub(stats.CurrentTime)
+
+		// Sleep until the next window starts.
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-clock.After(remainingTime):
+		}
+	}
+
 	return nil
 }
 
@@ -29,22 +61,22 @@ func (ts *FixedWindow) Throttle(context.Context) error {
 // Once the limit is exceeded, further requests are throttled until enough time has passed for requests to "fall out" of the window.
 type SlidingWindow struct{}
 
-type TokenBucket struct {
-	// Capacity defines the maximum number of tokens the bucket can hold. It acts as the upper limit on burstiness.
-	// A request consumes one token from the bucket. If there are no tokens left, the request will be throttled.
-	// This allows the system to handle bursts of requests up to the capacity, as long as tokens are available.
-	Capacity int
+// type TokenBucket struct {
+// 	// Capacity defines the maximum number of tokens the bucket can hold. It acts as the upper limit on burstiness.
+// 	// A request consumes one token from the bucket. If there are no tokens left, the request will be throttled.
+// 	// This allows the system to handle bursts of requests up to the capacity, as long as tokens are available.
+// 	Capacity int
 
-	// Refill defines the rate at which tokens are replenished in the bucket.
-	// Tokens are added to the bucket periodically, according to this rate (e.g., 5 tokens per second).
-	// Even after handling a burst, the bucket will gradually refill based on this rate, allowing more requests to be handled over time.
-	Refill Rate
+// 	// Refill defines the rate at which tokens are replenished in the bucket.
+// 	// Tokens are added to the bucket periodically, according to this rate (e.g., 5 tokens per second).
+// 	// Even after handling a burst, the bucket will gradually refill based on this rate, allowing more requests to be handled over time.
+// 	Refill Rate
 
-	// Leak [optional] defines a rate at which tokens or requests might be leaked out of the bucket.
-	// This allows for more fine-grained control, simulating the removal of tokens at a constant rate.
-	// If enabled, this prevents token accumulation when the system is underutilized, ensuring that the bucket doesn't stay full.
-	Leak Rate
-}
+// 	// Leak [optional] defines a rate at which tokens or requests might be leaked out of the bucket.
+// 	// This allows for more fine-grained control, simulating the removal of tokens at a constant rate.
+// 	// If enabled, this prevents token accumulation when the system is underutilized, ensuring that the bucket doesn't stay full.
+// 	Leak Rate
+// }
 
 // Rate expresses a rate of N tokens per a given duration.
 // This can be used both for refilling the TokenBucket and for determining the leak rate, if applicable.
@@ -53,4 +85,67 @@ type Rate struct {
 	N int
 	// Per defines the duration over which N tokens are added or leaked (e.g., 1 second or 1 minute).
 	Per time.Duration
+}
+
+func (r Rate) String() string {
+	return fmt.Sprintf("%d/%s", r.N, r.Per.String())
+}
+
+func (r Rate) IsZero() bool {
+	return r.N == 0 && r.Per == 0
+}
+
+type event struct {
+	Timestamp time.Time
+}
+
+type events struct {
+	es []event
+	m  sync.RWMutex
+}
+
+type stats struct {
+	WindowStart time.Time
+	CurrentTime time.Time
+	FirstAt     time.Time
+	N           int
+}
+
+func (st stats) String() string {
+	const format = "15:04:05"
+	return fmt.Sprintf("%d/%s [window start: %s | now: %s]", st.N, st.CurrentTime.Sub(st.WindowStart).String(),
+		st.WindowStart.Format(format), st.CurrentTime.Format(format))
+}
+
+func (es *events) Tick(r Rate) stats {
+	if r.IsZero() {
+		return stats{}
+	}
+
+	es.m.Lock()
+	defer es.m.Unlock()
+
+	currentTime := clock.Now()
+	windowStart := currentTime.Add(r.Per * -1)
+	firstAt := currentTime
+
+	// drop old entities
+	es.es = slicekit.Filter(es.es, func(e event) bool {
+		if e.Timestamp.Before(firstAt) {
+			firstAt = e.Timestamp
+		}
+		return windowStart.Equal(e.Timestamp) || windowStart.Before(e.Timestamp)
+	})
+
+	// add current occasion
+	es.es = append(es.es, event{Timestamp: currentTime})
+
+	st := stats{
+		WindowStart: windowStart,
+		CurrentTime: currentTime,
+		FirstAt:     firstAt,
+		N:           len(es.es),
+	}
+
+	return st
 }
--- pkg/throttling/throttling_test.go
+++ pkg/throttling/throttling_test.go
@@ -22,6 +22,8 @@ var rate = testcase.Var[throttling.Rate]{
 	},
 }
 
+const timeout = time.Second / 2
+
 func TestFixedWindow(t *testing.T) {
 	s := testcase.NewSpec(t)
 
@@ -31,68 +33,90 @@ func TestFixedWindow(t *testing.T) {
 		}
 	})
 
-	var CTX = let.Context(s)
+	var Context, ContextCancel = let.ContextWithCancel(s)
 
 	act := func(t *testcase.T) error {
-		return subject.Get(t).Throttle(CTX.Get(t))
+		return subject.Get(t).Throttle(Context.Get(t))
 	}
 
 	s.Before(func(t *testcase.T) {
-		timecop.Travel(t, time.Now(), timecop.Freeze) // time is not moving during the test
+		timecop.Travel(t, time.Now(), timecop.DeepFreeze) // time is not moving during the test
+	})
+
+	s.When("the context is cancelled", func(s *testcase.Spec) {
+		s.Before(func(t *testcase.T) {
+			ContextCancel.Get(t)()
+		})
+
+		s.Then("throttling returns with context's error", func(t *testcase.T) {
+			assert.ErrorIs(t, Context.Get(t).Err(), act(t))
+		})
 	})
 
 	s.When(".Rate left as empty value", func(s *testcase.Spec) {
 		rate.LetValue(s, throttling.Rate{})
 
 		s.Then("calling won't hangs due to inferred default values", func(t *testcase.T) {
-			assert.Within(t, time.Millisecond, func(ctx context.Context) {
+			assert.Within(t, timeout, func(ctx context.Context) {
 				assert.NoError(t, act(t))
 			})
 		})
 	})
 
 	s.Then("we can spike within the rate without throathling", func(t *testcase.T) {
-		assert.Within(t, time.Millisecond, func(ctx context.Context) {
+		assert.Within(t, timeout, func(ctx context.Context) {
 			spikeAct(t, act)
 		})
 	})
 
 	s.Then("we can make calls evenly during the time windows", func(t *testcase.T) {
-		assert.Within(t, time.Millisecond, func(ctx context.Context) {
+		assert.Within(t, timeout, func(ctx context.Context) {
 			pacedAct(t, act)
 		})
 	})
 
 	s.When("we already reached the rate limit are within the rate limit", func(s *testcase.Spec) {
 		s.Before(func(t *testcase.T) {
-			assert.Within(t, time.Second, func(ctx context.Context) {
+			assert.Within(t, timeout, func(ctx context.Context) {
 				pacedAct(t, act)
 			}, "arrangement had an issue, we probably got rate limited too early")
 		})
 
 		s.Then("rate limiting applies", func(t *testcase.T) {
-			w := assert.NotWithin(t, time.Second/2, func(ctx context.Context) {
+			w := assert.NotWithin(t, timeout, func(ctx context.Context) {
 				assert.NoError(t, act(t))
 			}, "expected that rate limit prevented it from returning early")
 
 			timecop.Travel(t, rate.Get(t).Per)
 
-			assert.Within(t, time.Millisecond, func(ctx context.Context) {
+			assert.Within(t, timeout, func(ctx context.Context) {
 				w.Wait()
 			}, "expected that after the rate limit window went away, the throttling ended")
+		})
+
+		s.And("the context is cancelled", func(s *testcase.Spec) {
+			s.Before(func(t *testcase.T) {
+				ContextCancel.Get(t)()
+			})
+
+			s.Then("throttling returns with context's error", func(t *testcase.T) {
+				assert.ErrorIs(t, Context.Get(t).Err(), act(t))
+			})
 		})
 	})
 }
 
 func pacedAct(t *testcase.T, act func(t *testcase.T) error) {
+	t.Helper()
 	pace := rate.Get(t).Per / time.Duration(rate.Get(t).N)
 	for i := 0; i < rate.Get(t).N; i++ {
 		assert.NoError(t, act(t))
-		timecop.Travel(t, pace, timecop.Freeze)
+		timecop.Travel(t, pace)
 	}
 }
 
 func spikeAct(t *testcase.T, act func(t *testcase.T) error) {
+	t.Helper()
 	timecop.SetSpeed(t, 0.1)
 	defer timecop.SetSpeed(t, 1)
 	for i := 0; i < rate.Get(t).N; i++ {
