2bed926944e99ddbee723b3b5ae94e21a76e4a32
Adam Luzsi <adamluzsi@gmail.com>
2025-03-14 21:39:27 +0100

    extrac stack lookup from errorkit into runtimekit
    Asserting on stack related actions are more generic,
    and easier to test as a seperate topic,
    therefore it is extracted from errorkit.

--- pkg/runtimekit/runtimekit.go
+++ pkg/runtimekit/runtimekit.go
@@ -1,9 +1,12 @@
 package runtimekit
 
 import (
+	"fmt"
+	"iter"
+	"math"
+	"reflect"
 	"runtime"
 	"strings"
-	"sync/atomic"
 	"unsafe"
 )
 
@@ -31,49 +34,131 @@ func ArchBitSize() int {
 	return int(sizeOfPointer * 8)
 }
 
-var exceptionsIndex int64
-var exceptions = map[int64]func(runtime.Frame) bool{}
+func OverStack() iter.Seq[runtime.Frame] {
+	return func(yield func(runtime.Frame) bool) {
+		var (
+			programCounters []uintptr
+			size            int
+			nCallers        int
+		)
 
-func RegisterTraceException(isException func(runtime.Frame) bool) func() {
-	var index int64
-	for range 1024 {
-		index = atomic.AddInt64(&exceptionsIndex, 1)
-		if _, ok := exceptions[index]; !ok {
-			break
-		}
-	}
-	if _, ok := exceptions[index]; ok {
-		panic("errorkit trace expection list is probably full")
-	}
-	exceptions[index] = isException
-	return func() { delete(exceptions, index) }
-}
+		// Dynamically expand the buffer until it can hold all callers.
+		for i := 0; ; i++ {
+			size = int(math.Pow(2, float64(i)))
+			programCounters = make([]uintptr, size)
+			nCallers = runtime.Callers(1 /* skip this frame */, programCounters)
 
-var _ = RegisterTraceException(func(f runtime.Frame) bool {
-	return strings.Contains(f.Function, "runtimekit.") ||
-		strings.Contains(f.Function, "runtime.") ||
-		strings.Contains(f.Function, "testing.")
-})
-
-func Stack() []runtime.Frame {
-	programCounters := make([]uintptr, 1024)
-	n_callers := runtime.Callers(1, programCounters)
-	frames := runtime.CallersFrames(programCounters[:n_callers])
-	var vs []runtime.Frame
-
-tracing:
-	for more := true; more; {
-		var frame runtime.Frame
-		frame, more = frames.Next()
-
-		for _, exception := range exceptions {
-			if exception(frame) {
-				continue tracing
+			if nCallers < len(programCounters) {
+				break // The buffer is sufficiently large
 			}
 		}
 
-		vs = append(vs, frame)
+		framesIter := runtime.CallersFrames(programCounters[:nCallers])
+
+	tracing:
+		for {
+			frame, more := framesIter.Next()
+			if !more {
+				break tracing
+			}
+			if isException(frame) {
+				continue
+			}
+			if !yield(frame) {
+				break tracing
+			}
+		}
+	}
+}
+
+func Stack() []runtime.Frame {
+	var stack []runtime.Frame
+	for frame := range OverStack() {
+		stack = append(stack, frame)
+	}
+	return stack
+}
+
+// FuncOf returns the runtime function metadata for the given function.
+//
+// It takes any func type value as input and attempts to resolve it to a runtime.Func.
+// If the input is not a function, it panics.
+// If the input is nil, it returns nil.
+func FuncOf(fn any) *runtime.Func {
+	if fn == nil {
+		return nil
+	}
+	v := reflect.ValueOf(fn)
+	if v.Kind() != reflect.Func {
+		panic(fmt.Sprintf("non-function kind: %s", v.Kind()))
+	}
+	pc := uintptr(v.UnsafePointer())
+	return runtime.FuncForPC(pc)
+}
+
+// FuncInfo is the information of a function type.
+//
+// IT's naming is based on the go language spec's terminology.
+// https://go.dev/ref/spec#Function_types
+type FuncInfo struct {
+	// ID is the function's identifier
+	ID string
+	// Receiver is the type's identifier in case of a method.
+	Receiver string
+	// Package is the name of the package where the function is declared.
+	Package string
+	// Import is the import path of the function's package.
+	// Import includes the package name as well
+	Import string
+	// IsMethodValue indicates if the function is a method value.
+	//
+	// In Go, methods are functions associated with a specific type and require
+	// a receiver (instance) to be called. When a method is assigned to a function
+	// variable (e.g., `m.MyMethod`), Go creates a *method value* — a closure function
+	// that "remembers" the receiver and can be invoked like a regular function.
+	//
+	// https://go.dev/ref/spec#Method_values
+	IsMethodValue bool
+}
+
+func FuncInfoOf(fn *runtime.Func) FuncInfo {
+	const ImportPathSeperator = "/"
+	var info FuncInfo
+	if fn == nil {
+		return info
+	}
+	var (
+		ids                  []string // [ package name ] | [ receiver type ] [ function name ]
+		funcName             = fn.Name()
+		indexOfLastSeperator = strings.LastIndex(funcName, ImportPathSeperator)
+	)
+	if 0 <= indexOfLastSeperator { // non top level package
+		identifiers := funcName[indexOfLastSeperator+1:]
+		ids = strings.Split(identifiers, ".")
+
+		info.Import = funcName[:indexOfLastSeperator+1] + ids[0]
+	} else { // top level stdlib
+		ids = strings.Split(funcName, ".")
+		info.Import = ids[0]
+	}
+	switch {
+	case len(ids) == 2:
+		info.Package = ids[0]
+		info.ID = ids[1]
+	case len(ids) == 3:
+		info.Package = ids[0]
+		info.ID = ids[2]
+		receiver := ids[1]
+		receiver = strings.TrimPrefix(receiver, "(")
+		receiver = strings.TrimSuffix(receiver, ")")
+		info.Receiver = receiver
 	}
 
-	return vs
+	const MethodValueSuffix = "-fm"
+	if strings.HasSuffix(info.ID, MethodValueSuffix) {
+		info.IsMethodValue = true
+		info.ID = strings.TrimSuffix(info.ID, MethodValueSuffix)
+	}
+
+	return info
 }
--- pkg/runtimekit/runtimekit_test.go
+++ pkg/runtimekit/runtimekit_test.go
@@ -9,6 +9,7 @@ import (
 
 	"go.llib.dev/frameless/pkg/runtimekit"
 	"go.llib.dev/testcase/assert"
+	"go.llib.dev/testcase/random"
 )
 
 func TestArchBitSize(t *testing.T) {
@@ -33,7 +34,7 @@ func TestRegisterTraceException(t *testing.T) {
 		assert.Contain(t, got.Function, t.Name())
 	})
 
-	undo := runtimekit.RegisterTraceException(func(f runtime.Frame) bool {
+	undo := runtimekit.RegisterFrameException(func(f runtime.Frame) bool {
 		return strings.Contains(f.Function, t.Name())
 	})
 
@@ -61,4 +62,101 @@ func TestStack(t *testing.T) {
 	assert.NoneOf(t, stack, func(t assert.It, frame runtime.Frame) {
 		assert.Contain(t, frame.File, "runtimekit.go")
 	}, "expected that runtimekit itself is not in the stack trace")
+
+	stubFunc(func() {
+		stack = runtimekit.Stack()
+	})
+
+	fn := runtimekit.FuncOf(stubFunc)
+
+	assert.OneOf(t, stack, func(t assert.It, frame runtime.Frame) {
+		assert.Contain(t, frame.Function, fn.Name())
+	})
+}
+
+// stubFunc
+//
+// DO NOT change the name of this function!
+func stubFunc(blk func()) { blk() }
+
+type TYPE struct{}
+
+func (TYPE) method() {}
+
+func (*TYPE) ptrMethod() {}
+
+func TestFuncOf(t *testing.T) {
+	t.Run("func", func(t *testing.T) {
+		fn := runtimekit.FuncOf(stubFunc)
+		assert.NotNil(t, fn)
+		assert.Equal(t, "go.llib.dev/frameless/pkg/runtimekit_test.stubFunc", fn.Name())
+	})
+	t.Run("method", func(t *testing.T) {
+		fn := runtimekit.FuncOf(TYPE.method)
+		assert.NotNil(t, fn)
+		assert.Equal(t, "go.llib.dev/frameless/pkg/runtimekit_test.TYPE.method", fn.Name())
+	})
+	t.Run("ptr-method", func(t *testing.T) {
+		var v TYPE
+		fn := runtimekit.FuncOf(v.ptrMethod)
+		assert.NotNil(t, fn)
+		assert.Equal(t, "go.llib.dev/frameless/pkg/runtimekit_test.(*TYPE).ptrMethod-fm", fn.Name())
+	})
+	t.Run("nil", func(t *testing.T) {
+		assert.Nil(t, runtimekit.FuncOf(nil))
+	})
+	t.Run("non function type will panic", func(t *testing.T) {
+		v := random.Pick[any](nil, "hello", 42, 24.42, true, struct{}{})
+
+		assert.NotNil(t, assert.Panic(t, func() {
+			runtimekit.FuncOf(v)
+		}))
+	})
+}
+
+func TestFuncInfoOf(t *testing.T) {
+	t.Run("func", func(t *testing.T) {
+		fn := runtimekit.FuncOf(stubFunc)
+		fni := runtimekit.FuncInfoOf(fn)
+		assert.Equal(t, fni.Import, "go.llib.dev/frameless/pkg/runtimekit_test")
+		assert.Equal(t, fni.Package, "runtimekit_test")
+		assert.Empty(t, fni.Receiver)
+		assert.Equal(t, fni.ID, "stubFunc")
+	})
+	t.Run("method-func", func(t *testing.T) {
+		fn := runtimekit.FuncOf(TYPE.method)
+		fni := runtimekit.FuncInfoOf(fn)
+		assert.Equal(t, fni.Import, "go.llib.dev/frameless/pkg/runtimekit_test")
+		assert.Equal(t, fni.Package, "runtimekit_test")
+		assert.Equal(t, fni.Receiver, "TYPE")
+		assert.Equal(t, fni.ID, "method")
+		assert.False(t, fni.IsMethodValue)
+	})
+	t.Run("method-value", func(t *testing.T) {
+		var v TYPE
+		fn := runtimekit.FuncOf(v.method)
+		fni := runtimekit.FuncInfoOf(fn)
+		assert.Equal(t, fni.Import, "go.llib.dev/frameless/pkg/runtimekit_test")
+		assert.Equal(t, fni.Package, "runtimekit_test")
+		assert.Equal(t, fni.Receiver, "TYPE")
+		assert.Equal(t, fni.ID, "method")
+		assert.True(t, fni.IsMethodValue)
+	})
+	t.Run("prt-method", func(t *testing.T) {
+		var v TYPE
+		fn := runtimekit.FuncOf(v.ptrMethod)
+		fni := runtimekit.FuncInfoOf(fn)
+		assert.Equal(t, fni.Import, "go.llib.dev/frameless/pkg/runtimekit_test")
+		assert.Equal(t, fni.Package, "runtimekit_test")
+		assert.Equal(t, fni.Receiver, "*TYPE")
+		assert.Equal(t, fni.ID, "ptrMethod")
+	})
+	t.Run("top level package without any importpath", func(t *testing.T) {
+		fn := runtimekit.FuncOf(testing.TB.Name)
+		fni := runtimekit.FuncInfoOf(fn)
+		assert.Equal(t, fni.Package, "testing")
+		assert.Equal(t, fni.Import, "testing")
+		assert.Equal(t, fni.Receiver, "TB")
+		assert.Equal(t, fni.ID, "Name")
+	})
 }
