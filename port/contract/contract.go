a01c82e3def6f4e7e19b7ba1f38b0a58c25edd95
Adam Luzsi <adamluzsi@gmail.com>
2025-08-26 20:51:14 +0200

    update documentation for contract port

--- port/contract/contract.go
+++ port/contract/contract.go
@@ -1,8 +1,23 @@
 package contract
 
 import (
+	"testing"
+
 	"go.llib.dev/testcase"
 )
+
+// Make func meant to create a new instance of the testing subject.
+// In testing suites, we often focus on the type of interface that is being examined.
+//
+// When a test requires multiple specifications—so simple option idioms aren't sufficient,
+// we can create a "XXXSubject" struct that contains all the necessary dependencies as fields.
+// This approach lets us use a single ‘Make" function to set up each testing case within a contract,
+// while keeping the configuration extensible in an open‑closed‑principle style.
+//
+// The "Make" function should be called only once and must provide a field capable of creating the interface value.
+//
+// Feel free to explore this pattern for your testing needs.
+type Make[Subject any] = func(tb testing.TB) Subject
 
 // Contract represents a resource specification also known as "contract".
 //
@@ -18,5 +33,14 @@ import (
 // instead of going into implementation details.
 type Contract interface {
 	testcase.Suite
-	testcase.OpenSuite
+	// Test is the function that assert expected behavioral requirements from a supplier implementation.
+	// These behavioral assumptions made by the Consumer in order to simplify and stabilise its own code complexity.
+	// Every time a Consumer makes an assumption about the behavior of the role interface supplier,
+	// it should be clearly defined it with tests under this functionality.
+	Test(*testing.T)
+	// Benchmark will help with what to measure.
+	// When you define a role interface contract, you should clearly know what performance aspects important for your Consumer.
+	// Those aspects should be expressed in a form of Benchmark,
+	// so different supplier implementations can be easily A/B tested from this aspect as well.
+	Benchmark(*testing.B)
 }
