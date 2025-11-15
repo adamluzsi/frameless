3aa5adbf1cc892295dd44fd7e43448044970f301
Adam Luzsi <adamluzsi@gmail.com>
2025-11-15 02:03:46 +0100

    devops health pkg QoL

---
+++ pkg/devops/health/deprecated.go
@@ -0,0 +1,11 @@
+package health
+
+// Check
+//
+// Deprecated: use IssueCheck type instead.
+type Check IssueCheck
+
+// DependencyCheck is a HealthCheck of a given DependencyCheck.
+//
+// Deprecated: use HealthCheck type instead.
+type DependencyCheck HealthCheck
