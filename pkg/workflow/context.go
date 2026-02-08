86bfa18cfc0c9d2af4c832e65cf4a1deb57e247d
Adam Luzsi <adamluzsi@gmail.com>
2026-02-08 21:55:14 +0100

    wip

---
+++ pkg/workflow/context.go
@@ -0,0 +1,26 @@
+package workflow
+
+import (
+	"context"
+
+	"go.llib.dev/frameless/pkg/contextkit"
+	"go.llib.dev/frameless/pkg/mapkit"
+)
+
+var ctxConfigH contextkit.ValueHandler[ctxConfigHKey, ctxConfig]
+
+type ctxConfigHKey struct{}
+
+type ctxConfig struct {
+	Participants ParticipantRepository
+	FuncMap      TemplateFuncMap
+}
+
+func ContextWithFuncMap(ctx context.Context, fm TemplateFuncMap) context.Context {
+	if len(fm) == 0 {
+		return ctx
+	}
+	c, _ := ctxConfigH.Lookup(ctx)
+	c.FuncMap = mapkit.Merge(c.FuncMap, fm)
+	return ctxConfigH.ContextWith(ctx, c)
+}
