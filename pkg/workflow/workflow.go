a4f8af372479885506fb1cfb757f2e39535058b9
Adam Luzsi <adamluzsi@gmail.com>
2026-03-19 01:23:58 +0100

    wip

--- pkg/workflow/workflow.go
+++ pkg/workflow/workflow.go
@@ -4,14 +4,17 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"sync"
 
-	"go.llib.dev/frameless/pkg/mapkit"
+	"go.llib.dev/frameless/internal/errorkitlite"
 	"go.llib.dev/frameless/pkg/reflectkit"
 	"go.llib.dev/frameless/pkg/validate"
-	"go.llib.dev/frameless/port/ds"
+	"go.llib.dev/frameless/port/crud"
 	"go.llib.dev/frameless/port/ds/dsmap"
 )
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 type Definition interface {
 	minDefinition
@@ -26,9 +29,19 @@ type minDefinition interface {
 	// validate.Validatable
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 type Condition interface {
 	minCondition
 	minDefinition
+}
+
+type ConditionID string
+
+type ErrConditionNotFound struct{ CID ConditionID }
+
+func (e ErrConditionNotFound) Error() string {
+	return fmt.Sprintf("[ErrConditionNotFound] %s", e.CID)
 }
 
 var _ minCondition = (Condition)(nil)
@@ -37,6 +50,23 @@ type minCondition interface {
 	Evaluate(ctx context.Context, p *Process) (bool, error)
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type Runtime struct {
+	Participants ParticipantRepository
+	Conditions   ConditionRepository
+}
+
+type ParticipantRepository interface {
+	crud.ByIDFinder[Participant, ParticipantID]
+}
+
+type ConditionRepository interface {
+	crud.ByIDFinder[Condition, ConditionID]
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 type Process struct {
 	Definition Definition `json:"pdef"`
 	Variables  Variables  `json:"var"`
@@ -44,16 +74,28 @@ type Process struct {
 	m sync.Mutex
 }
 
-// TODO: rename to Process
-type State struct {
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type Variables struct {
 	vs dsmap.Map[VariableKey, any]
 }
 
-func (s *State) Validate(ctx context.Context) error {
-	return s.validateVariables(ctx)
+type VariableKey string
+
+func (vs Variables) Validate(ctx context.Context) error {
+	return vs.validateVariables(ctx)
 }
 
-func (s *State) validateVariables(ctx context.Context) error {
+func (vs *Variables) Merge(oth Variables) {
+	if oth.vs == nil {
+		return
+	}
+	for k, v := range oth.vs.All() {
+		vs.vs.Set(k, v)
+	}
+}
+
+func (s *Variables) validateVariables(ctx context.Context) error {
 	data, err := json.Marshal(s.vs)
 	if err != nil {
 		return fmt.Errorf("workflow variables are not valid because the values must be json encodable")
@@ -68,23 +110,58 @@ func (s *State) validateVariables(ctx context.Context) error {
 	return nil
 }
 
-func (s *State) Merge(oth *State) {
-	if s == nil {
-		panic(fmt.Sprintf("nil %T", s))
-	}
-	if oth == nil {
-		return
-	}
-	for k, v := range mapkit.Merge(s.vs, oth.vs) {
-		s.vs.Set(k, v)
-	}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Participant is a logical unit implemented at workflow engine-level.
+//
+// If ParticipantRepository is supplied to the workflow runtime context,
+// then registered particpants can be used from within workflow definitions.
+type Participant struct {
+	ID   ParticipantID
+	Func any // func(context.Context, ...) (..., error)
 }
 
-type Variables struct {
-	vs ds.Map[VariableKey, any]
+type ParticipantID string
+
+var _ validate.Validatable = (*Participant)(nil)
+
+const ErrInvalidParicipantFunc errorkitlite.Error = `Invalid workflow.Participant#Func signature:
+expected func(context.Context, arg1 T1, ...OtherArgs) (Result1, Result2..., error)
+where the function signature starts with a context.Context, then user defined argument types,
+and the results tuple is also returns user defined types, ending with an error value type.
+The input and output argument types must be serializable.
+`
+
+var reflectContextType = reflectkit.TypeOf[context.Context]()
+
+var reflectErrorType = reflectkit.TypeOf[error]()
+
+func (p Participant) Validate(ctx context.Context) error {
+	rfunc := reflect.ValueOf(p.Func)
+	if rfunc.Kind() != reflect.Func {
+		return fmt.Errorf("invalid value for participant func")
+	}
+	var (
+		funcType   = rfunc.Type()
+		funcNumIn  = funcType.NumIn()
+		funcNumOut = funcType.NumOut()
+	)
+	if funcNumIn < 1 {
+		return ErrInvalidParicipantFunc
+	}
+	if funcType.In(0) != reflectContextType {
+		return ErrInvalidParicipantFunc
+	}
+	if funcNumOut < 1 {
+		return ErrInvalidParicipantFunc
+	}
+	if funcType.Out(funcNumOut-1) != reflectErrorType {
+		return ErrInvalidParicipantFunc
+	}
+	return nil
 }
 
-type VariableKey string
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 type ParticipantMapping map[ParticipantID]any
 
@@ -112,15 +189,7 @@ func (ps ParticipantMapping) Validate(ctx context.Context) error {
 	return nil
 }
 
-func vd(ctx context.Context, name string, v validate.Validatable, req bool) error {
-	if req && v == nil {
-		return validate.Error{Cause: fmt.Errorf("%s is missing", name)}
-	}
-	if v == nil {
-		return nil
-	}
-	return v.Validate(ctx)
-}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 type JSONSerialisable interface {
 	json.Marshaler
