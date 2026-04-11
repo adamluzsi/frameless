cfdd08fc3c7150b298ded3a4e383689eaed034a9
Adam Luzsi <adamluzsi@gmail.com>
2026-04-11 07:40:33 +0200

    wip

--- pkg/workflow/condition.go
+++ pkg/workflow/condition.go
@@ -1,1 +1,12 @@
 package workflow
+
+import "context"
+
+type ExecuteCondition struct {
+	ID    ConditionID   `json:"id"`
+	Input []VariableKey `json:"input,omitempty"`
+}
+
+func (ec ExecuteCondition) Evaluate(ctx context.Context, p *Process) (bool, error) {
+	return false, nil
+}
