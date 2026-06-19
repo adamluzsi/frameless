e4870d700494889910bcb55f8e7ad81976901a38
Adam Luzsi <adamluzsi@gmail.com>
2026-06-19 15:30:31 +0200

    wip

---
+++ pkg/logging/duration_test.go
@@ -0,0 +1,17 @@
+package logging_test
+
+import (
+	"testing"
+
+	"go.llib.dev/frameless/pkg/logging"
+)
+
+func ExampleDuration() {
+
+	start := logging.Duration()
+
+}
+
+func TestDuration(t *testing.T) {
+
+}
